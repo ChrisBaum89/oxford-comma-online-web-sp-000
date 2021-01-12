e87e6629fdb7058ba0dd4a71b5c5f1989492f5c7
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 00:55:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,7 +12,8 @@ def oxford_comma(array)
       last_index_count = num_of_elements - 1
       array_last_element = array
       array_last_element[last_index_count] = " and " + array.last
-      puts "#{array_last_element}"
+      #puts "#{array_last_element}"
+      array_last_element.join(", ")
   end
 end
 
